8c6e226c02264dab6acc326a934766f9ccd4b842
Raniery Almeida de Oliveira <ranie-ry@outlook.com>
2026-08-01 15:12:25 -0300

    feat: added new models and reworked mouse controls

--- scenes/drag_n_drop_controller.gd
+++ scenes/drag_n_drop_controller.gd
@@ -3,7 +3,8 @@ extends Node3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
-@export_flags_3d_physics var mouse_layer_mask: int
+@export_flags_3d_physics var ground_layer_mask: int
+@export_flags_3d_physics var moveable_layer_mask: int
 
 var draggingCollider: MoveableBody
 var mousePosition: Vector3
@@ -13,8 +14,9 @@ func _input(event: InputEvent):
 	var intersect: Dictionary
 	
 	if event is InputEventMouse:
-		intersect = get_mouse_intersect(event.position)
-		if intersect: mousePosition = Vector3(intersect.position.x, 0.1, intersect.position.z)
+		intersect = get_mouse_intersect(event.position, ground_layer_mask)
+		if intersect:
+			mousePosition = Vector3(intersect.position.x, intersect.position.y + 0.1, intersect.position.z)
 		#snap on collider
 		#if intersect: mousePosition = intersect.collider.global_position
 		
@@ -26,12 +28,14 @@ func _input(event: InputEvent):
 			doDrag = false
 			drag_and_drop(intersect)
 		elif leftButtonPressed:
+			var intersect_moveable = get_mouse_intersect(event.position, moveable_layer_mask)
+			
 			doDrag = true
-			drag_and_drop(intersect)
+			drag_and_drop(intersect_moveable)
 
 func _physics_process(delta: float) -> void:
 	if draggingCollider:
-		draggingCollider.global_position = mousePosition.clamp(Vector3(-5, 0, -4), Vector3(5, 1, 4))
+		draggingCollider.model.global_position = mousePosition.clamp(Vector3(-2, 0, -2), Vector3(2, 1, 2))
 
 func drag_and_drop(intersect: Dictionary):
 	if intersect.is_empty():
@@ -42,18 +46,22 @@ func drag_and_drop(intersect: Dictionary):
 	if !draggingCollider && doDrag && canMove:
 		draggingCollider = intersect.collider
 		draggingCollider.is_dragging = true
+		draggingCollider.set_physics_process(false)
 	elif draggingCollider:
+		draggingCollider.global_position = draggingCollider.model.global_position
+		draggingCollider.model.position = Vector3.ZERO
+		draggingCollider.set_physics_process(true)
 		draggingCollider.is_dragging = false
 		draggingCollider = null
 
 	
-func get_mouse_intersect(mousePosition: Vector2) -> Dictionary:
+func get_mouse_intersect(mousePosition: Vector2, layer_mask: int) -> Dictionary:
 	var currentCamera := get_viewport().get_camera_3d()
 	var params := PhysicsRayQueryParameters3D.new()
 	
 	params.from = currentCamera.project_ray_origin(mousePosition)
 	params.to = currentCamera.project_position(mousePosition, 1000)
-	params.collision_mask = mouse_layer_mask
+	params.collision_mask = layer_mask
 	if draggingCollider: params.exclude = [draggingCollider]
 	
 	var worldspace = get_world_3d().direct_space_state
--- scenes/draggable_body.gd
+++ scenes/draggable_body.gd
@@ -3,14 +3,18 @@ class_name MoveableBody extends CharacterBody3D
 const SPEED = 30
 
 @export var pose: CharacterScoreResource.CharacterPose
+@export var model: Node3D
 
+var animation_player: AnimationPlayer
 var is_dragging: bool
+
+func _ready() -> void:
+	animation_player = model.find_child("AnimationPlayer")
 
 func _physics_process(delta: float) -> void:	
 	if not is_on_floor() and not is_dragging:
 		velocity += get_gravity() * delta
-	
-	velocity.x = move_toward(velocity.x, 0, SPEED * delta)
-	velocity.z = move_toward(velocity.z, 0, SPEED * delta)
+		velocity.x = move_toward(velocity.x, 0, SPEED * delta)
+		velocity.z = move_toward(velocity.z, 0, SPEED * delta)
 	
 	move_and_slide()
